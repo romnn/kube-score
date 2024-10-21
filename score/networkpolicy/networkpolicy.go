9d1feece245fc9350d6636f936ebc9883f59e0dd
romnn <roman.dahm@gmx.de>
2024-10-22 00:25:53 +0200

    fix: network policy check uses namespace flag for pod specers

--- score/networkpolicy/networkpolicy.go
+++ score/networkpolicy/networkpolicy.go
@@ -1,6 +1,8 @@
 package networkpolicy
 
 import (
+	"fmt"
+
 	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -15,18 +17,29 @@ type Options struct {
 }
 
 func Register(allChecks *checks.Checks, netpols ks.NetworkPolicies, pods ks.Pods, podspecers ks.PodSpeccers, options Options) {
-	allChecks.RegisterPodCheck("Pod NetworkPolicy", `Makes sure that all Pods are targeted by a NetworkPolicy`, podHasNetworkPolicy(netpols.NetworkPolicies(), options))
-	allChecks.RegisterNetworkPolicyCheck("NetworkPolicy targets Pod", `Makes sure that all NetworkPolicies targets at least one Pod`, networkPolicyTargetsPod(pods.Pods(), podspecers.PodSpeccers(), options))
+	allChecks.RegisterPodCheck(
+		"Pod NetworkPolicy",
+		`Makes sure that all Pods are targeted by a NetworkPolicy`,
+		podHasNetworkPolicy(netpols.NetworkPolicies(), options),
+	)
+	allChecks.RegisterNetworkPolicyCheck(
+		"NetworkPolicy targets Pod",
+		`Makes sure that all NetworkPolicies targets at least one Pod`,
+		networkPolicyTargetsPod(pods.Pods(), podspecers.PodSpeccers(), options),
+	)
 }
 
 // podHasNetworkPolicy returns a function that tests that all pods have matching NetworkPolicies
 // podHasNetworkPolicy takes a list of all defined NetworkPolicies as input
 func podHasNetworkPolicy(allNetpols []ks.NetworkPolicy, options Options) func(ks.PodSpecer) (scorecard.TestScore, error) {
+	verbose := false
 	return func(ps ks.PodSpecer) (score scorecard.TestScore, err error) {
 		hasMatchingEgressNetpol := false
 		hasMatchingIngressNetpol := false
 
-		podNamespace := ps.GetPodTemplateSpec().Namespace
+		pod := ps.GetPodTemplateSpec()
+
+		podNamespace := pod.Namespace
 		if podNamespace == "" {
 			podNamespace = options.Namespace
 		}
@@ -40,21 +53,32 @@ func podHasNetworkPolicy(allNetpols []ks.NetworkPolicy, options Options) func(ks
 				netPolNamespace = options.Namespace
 			}
 
-			// fmt.Printf(
-			// 	"policyNamespace=%s/%s podNamespace=%s/%s\n",
-			// 	netPolNamespace,
-			// 	netPol.Name,
-			// 	podNamespace,
-			// 	ps.GetPodTemplateSpec().Name,
-			// )
+			if verbose {
+				fmt.Printf(
+					"NAMESPACES:\n\t%80s = %s selector=%v\n\t%80s = %s labels=%v\n",
+					fmt.Sprintf("policy/%s", netPol.Name),
+					netPolNamespace,
+					netPol.Spec.PodSelector,
+					fmt.Sprintf("pod/%s", pod.Name),
+					podNamespace,
+					pod.Labels,
+				)
+			}
 
 			// Make sure that the pod and networkpolicy is in the same namespace
 			if podNamespace != netPolNamespace {
 				continue
 			}
 
+			if verbose {
+				fmt.Printf("policy/%s => NAMESPACE match\n", netPol.Name)
+			}
 			if selector, err := metav1.LabelSelectorAsSelector(&netPol.Spec.PodSelector); err == nil {
-				if selector.Matches(internal.MapLabels(ps.GetPodTemplateSpec().Labels)) {
+				if selector.Matches(internal.MapLabels(pod.Labels)) {
+
+					if verbose {
+						fmt.Printf("policy/%s => LABEL match\n", netPol.Name)
+					}
 
 					// Documentation of PolicyTypes
 					//
@@ -106,6 +130,7 @@ func podHasNetworkPolicy(allNetpols []ks.NetworkPolicy, options Options) func(ks
 }
 
 func networkPolicyTargetsPod(pods []ks.Pod, podspecers []ks.PodSpecer, options Options) func(networkingv1.NetworkPolicy) (scorecard.TestScore, error) {
+	verbose := false
 	return func(netpol networkingv1.NetworkPolicy) (score scorecard.TestScore, err error) {
 		hasMatch := false
 
@@ -122,19 +147,30 @@ func networkPolicyTargetsPod(pods []ks.Pod, podspecers []ks.PodSpecer, options O
 				podNamespace = options.Namespace
 			}
 
-			// fmt.Printf(
-			// 	"policyNamespace=%s/%s podNamespace=%s/%s\n",
-			// 	netPolNamespace,
-			// 	netpol.Name,
-			// 	podNamespace,
-			// 	pod.Name,
-			// )
+			if verbose {
+				fmt.Printf(
+					"NAMESPACES:\n\t%80s = %s selector=%v\n\t%80s = %s labels=%v\n",
+					fmt.Sprintf("policy/%s", netpol.Name),
+					netPolNamespace,
+					netpol.Spec.PodSelector,
+					fmt.Sprintf("pod/%s", pod.Name),
+					podNamespace,
+					pod.Labels,
+				)
+			}
 			if podNamespace != netPolNamespace {
 				continue
 			}
 
+			if verbose {
+				fmt.Printf("policy/%s => NAMESPACE match\n", netpol.Name)
+			}
+
 			if selector, err := metav1.LabelSelectorAsSelector(&netpol.Spec.PodSelector); err == nil {
 				if selector.Matches(internal.MapLabels(pod.Labels)) {
+					if verbose {
+						fmt.Printf("policy/%s => LABEL match\n", netpol.Name)
+					}
 					hasMatch = true
 					break
 				}
@@ -143,12 +179,24 @@ func networkPolicyTargetsPod(pods []ks.Pod, podspecers []ks.PodSpecer, options O
 
 		if !hasMatch {
 			for _, pod := range podspecers {
-				if pod.GetObjectMeta().Namespace != netPolNamespace {
+				podNamespace := pod.GetObjectMeta().Namespace
+				if podNamespace == "" {
+					podNamespace = options.Namespace
+				}
+
+				if podNamespace != netPolNamespace {
 					continue
+				}
+
+				if verbose {
+					fmt.Printf("policy/%s => NAMESPACE match\n", netpol.Name)
 				}
 
 				if selector, err := metav1.LabelSelectorAsSelector(&netpol.Spec.PodSelector); err == nil {
 					if selector.Matches(internal.MapLabels(pod.GetPodTemplateSpec().Labels)) {
+						if verbose {
+							fmt.Printf("policy/%s => LABEL match\n", netpol.Name)
+						}
 						hasMatch = true
 						break
 					}
